69ac5c56344a32729f5421c4993fee28cc61184c
Vadim Gul <vad_gul2005@mail.ru>
2024-12-13 17:46:28 +0300

    add UserControlller, ServerStatusController

---
+++ Controllers/ServerStatusController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GenAPassBackend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ServerStatusController : ControllerBase
+    {
+        [HttpGet("status")]
+        public IActionResult GetStatus()
+        {
+            return Ok(new { status = "Server is running", timestamp = System.DateTime.UtcNow });
+        }
+    }
+}
